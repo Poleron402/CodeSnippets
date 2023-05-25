4c7a76bdea3aa32761f784c77f9bbcfe7a80c675
Poleron402 <89750832+Poleron402@users.noreply.github.com>
2023-05-25 06:27:41 -0700

    First commit

---
+++ Reverse_String.py
@@ -0,0 +1,14 @@
+#this is the test code snipped where I reverse a string that the user has entered
+
+user_str = input("Enter anything<< ")
+def reverse_Operation(user_str):
+    user_str_rev = ""
+    if user_str != "":
+        for i in user_str[::-1]:
+            user_str_rev+=i
+        print(user_str_rev)
+    else:
+        print("You have entered an empty string.")
+
+
+
